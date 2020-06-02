d9e7f359085d465250ddd40193524e2137451c93
maiya <maiya@ecook.cn>
2020-06-02 09:11:27 +0800

    misc:第一次提交代码

---
+++ reducing_cook/android/app/src/main/kotlin/cn/ecook/ecook/MainActivity.kt
@@ -0,0 +1,6 @@
+package cn.ecook.ecook
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ reducing_cook/lib/news/renews.dart
@@ -0,0 +1,1 @@
+import 'package:flutter/material.dart';class newsVC extends StatelessWidget {  @override  Widget build(BuildContext context) {    // TODO: implement build    return Scaffold(      appBar: AppBar(        title: Text('资讯'),      ),    );  }}
