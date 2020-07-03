62b0f4a97f26c587c697fa7abb1b54d4b6e169f0
maiya <maiya@ecook.cn>
2020-07-03 22:12:49 +0800

    misc:暂存

---
+++ reducing_cook/lib/model/menu_detail_model.dart
@@ -0,0 +1,187 @@
+/// hasVideo : false
+/// imageid : "260119216"
+/// collectCount : "229"
+/// description : "采用天然健康的食材，不加糖不加盐不用油，而且没有添加泡打粉，搭配坚果和水果，健康减脂早餐开启美好新一天~ 因为健康无添加而且松软，小宝宝也可以吃哦~"
+/// likeCount : "32"
+/// authorid : "257500674"
+/// type : "0"
+/// commentCount : "14"
+/// tags : ["健康","减肥","松饼","水果","炒","牛奶","面粉","鸡蛋"]
+/// videourl : ""
+/// authorname : "小菁同学"
+/// name : "无油无糖香蕉松饼"
+/// exclusive : 0
+/// id : "260119304"
+/// viewCount : "810"
+/// authorimageid : "262728618"
+
+import 'package:json_annotation/json_annotation.dart';
+
+/// name : "减脂健身"
+/// state : "200"
+/// list : [{"imageid":"264237719","videoUrl":null,"materials":"玉米渣,大米,枸杞","name":"玉米渣粥","description":"玉米渣可以用来煮饭和煲粥，具有通便、护眼、降压降脂、补脑的功效。","recommend":"","id":"264237722","user":{"imageid":"264065389","star":0,"profile":"请添加您的个人简介","nickname":"薯条沾腐乳","signed":0,"id":"263277981","type":"Rookie"},"infos":{"hasVideo":false,"grade":"1","collectionCount":41,"exclusive":0,"likeCount":37,"viewCount":668,"isrec":"4"},"status":1},{"imageid":"262493438","materials":"马蹄,虾仁,盐,料酒,鸡精,胡萝卜,青豆,小碗秋耳,食用油,葱","name":"马蹄炒虾仁——时令快手养生菜","description":"马蹄又称荸荠，肉质洁白，味甜多汁，清脆可口，自古有地下雪梨之美誉，具有清肺热，又富含黏液质，有生津润肺、化痰利肠、通淋利尿、消痈解毒、凉血化湿、消食除胀的功效。\n马蹄的脆、甜遇上鲜虾的鲜，在味道和口感上都能给人带来惊喜，鲜美清甜的这道菜绝对不容错过！","recommend":"","id":"262493483","user":{"imageid":"258807042","star":5,"profile":"美食合作加V:wheric962464（备注网上厨房）","nickname":"沙小囡","signed":0,"id":"258498484","type":"Rookie"},"infos":{"hasVideo":false,"grade":"1","collectionCount":562,"exclusive":0,"likeCount":49,"viewCount":946,"isrec":"4"},"status":1},{"imageid":"262832125","materials":"蒜苔,香干,大红椒,蒜粒","name":"超下饭神器蒜苔香干粒","description":"蒜苔炒香干，香干嫩，蒜苔香，是米饭的好搭档，食材简单，还简单快手，几分钟就可以出锅，家常菜首选哦～","recommend":"","id":"262832131","user":{"imageid":"262095154","star":0,"profile":"","nickname":"西蘭","signed":0,"id":"260839089","type":"Rookie"},"infos":{"hasVideo":false,"grade":"1","collectionCount":679,"exclusive":1,"likeCount":42,"viewCount":1439,"isrec":"4"},"status":1},{"imageid":"264604378","materials":"腐竹,青椒,红椒,猪肉,蚝油,生抽,料酒,盐,蔬之鲜,香葱","name":"青椒炒腐竹","description":"青椒炒腐竹超级下饭，而且做法也简单，喜欢的朋友们可以收藏做起来哦","recommend":"","id":"264585303","user":{"imageid":"243079439","star":5,"profile":"为爱下厨房，做个美厨娘🌹","nickname":"妙江南","signed":1,"id":"23754959","type":"Verified"},"infos":{"hasVideo":false,"grade":"1","collectionCount":37,"exclusive":0,"likeCount":32,"viewCount":553,"isrec":"4"},"status":1},{"imageid":"263037267","materials":"绿豆芽,五花肉,韭菜,小米辣,大蒜,姜,粉条,花生油,洋葱,老醋,花椒粉,生抽,盐,鸡精","name":"五花肉醋溜绿豆芽","description":"蔬菜中最喜欢吃绿豆芽，因其口感清爽美味，营养又丰富，加了粉条和五花肉，用醋溜的方法，炒的绿豆芽，酸爽可口，绿豆芽中的维生素C，膳食纤维都非常丰富，能排毒养颜，促进肠胃蠕动，消脂排便，再加上绿豆芽热量低，因此要减肥瘦身，排毒养颜的女性，可以时常吃点热量低的绿豆芽。","recommend":"","id":"263037278","user":{"imageid":"262900317","star":5,"profile":"爱美食爱分享","nickname":"月色家的小厨房","signed":1,"id":"23770403","type":"Rookie"},"infos":{"hasVideo":false,"grade":"1","collectionCount":126,"exclusive":0,"likeCount":35,"viewCount":840,"isrec":"4"},"status":1},{"imageid":"263075533","materials":"秋葵,小米辣,蒜头,盐,食用油,白糖,蚝油,生抽,鸡精","name":"凉拌秋葵","description":"今天给大家分享一道凉拌秋葵，它的营养非常丰富，富含蛋白质、钙、铁、磷等，它的嫩荚还含有丰富的果胶、膳食纤维，其果胶的粘稠状态是植物界非常少见的，膳食纤维的占比及其中可溶性膳食纤维占比均是植物中非常高的， 所以秋葵具有非常好的清肠吸油和减肥的神奇作用，爱美的女人都喜欢吃它，又有营养还能减肥。它是一种高蛋白，高营养，低脂肪，低热量，无胆固醇的减肥佳品，适合所有减肥人群食用。","recommend":"","id":"263075534","user":{"imageid":"260475987","star":5,"profile":"爱家人，爱美食，爱分享！所有菜谱均为原创！","nickname":"紫色勿忘我美食","signed":0,"id":"4794696","type":"Rookie"},"infos":{"hasVideo":false,"grade":"1","collectionCount":248,"exclusive":0,"likeCount":36,"viewCount":1071,"isrec":"4"},"status":1},{"imageid":"262742390","materials":"茨菇,青蒜,炸酱,姜片,大豆油","name":"炸酱焖茨菇","description":"这个菜谱还是春天拍的，一直没有时间编辑，虽说现在不是吃茨菇的最好季节，还是发了完事。","recommend":"","id":"262742401","user":{"imageid":"260538567","star":5,"profile":"","nickname":"画画-eakr","signed":0,"id":"255049987","type":"Verified"},"infos":{"hasVideo":false,"grade":"1","collectionCount":38,"exclusive":0,"likeCount":29,"viewCount":449,"isrec":"4"},"status":1},{"imageid":"262952882","materials":"芹菜叶,面粉,玉米粉,油","name":"蒸芹菜叶","description":"芹菜浑身都是宝，芹菜营养价值高含有膳食纤维，有降血压的功效，蒸菜成了餐桌上的宠儿，粉蒸芹菜叶、粉蒸马齿苋、粉蒸地瓜叶……沾上料汁味道棒极了  。 把芹菜叶裹上生粉蒸一下，然后拌上醋和蒜泥，简直好吃的不得了，加工过后的芹菜也也不会再苦啦！","recommend":"","id":"262952774","user":{"imageid":"257076341","star":0,"profile":"合作请加味心：xiaozi-0118","nickname":"个性胜过姿色","signed":0,"id":"246133076","type":"Rookie"},"infos":{"hasVideo":false,"grade":"1","collectionCount":273,"exclusive":0,"likeCount":50,"viewCount":2037,"isrec":"4"},"status":1},{"imageid":"264406365","materials":"田螺,辣椒🌶️,花椒,老干妈,料酒,白酒,清水,食用油,味精,盐","name":"麻辣田螺","description":"去大排档，最喜欢点的就是麻辣田螺，吮吸起来有滋有味，再嘬嘬手指，哈哈……满满的世景味！","recommend":"","id":"264406366","user":{"imageid":"264405170","star":5,"profile":"静心感受自然，一草一木皆艺术；食物，与生俱来就是天然的艺术品","nickname":"茗茗爱美食","signed":0,"id":"263244534","type":"Verified"},"infos":{"hasVideo":false,"grade":"1","collectionCount":29,"exclusive":1,"likeCount":27,"viewCount":396,"isrec":"4"},"status":1},{"imageid":"264527023","materials":"鲍鱼,粉丝,蒜瓣,蚝油,酱油,料酒,白糖,食用油,清水","name":"蒜蓉粉丝蒸鲍鱼","description":"立夏已过，又到吃各种海鲜的时候，蒜蓉粉丝蒸鲍鱼，蒜香味浓郁，鲍鱼脆嫩！","recommend":"","id":"264527024","user":{"imageid":"264405170","star":5,"profile":"静心感受自然，一草一木皆艺术；食物，与生俱来就是天然的艺术品","nickname":"茗茗爱美食","signed":0,"id":"263244534","type":"Verified"},"infos":{"hasVideo":false,"grade":"1","collectionCount":41,"exclusive":1,"likeCount":35,"viewCount":309,"isrec":"4"},"status":1}]
+/// message : "获取成功！"
+
+///ß
+///
+///
+///
+class HomeBean {
+  String name;
+  String state;
+  List<ListBean> list;
+  String message;
+
+  static HomeBean fromMap(Map<String, dynamic> map) {
+    if (map == null) return null;
+    HomeBean homeBeanBean = HomeBean();
+    homeBeanBean.name = map['name'];
+    homeBeanBean.state = map['state'];
+    homeBeanBean.list = List()
+      ..addAll((map['list'] as List ?? []).map((o) => ListBean.fromMap(o)));
+    homeBeanBean.message = map['message'];
+    return homeBeanBean;
+  }
+
+  Map toJson() => {
+    "name": name,
+    "state": state,
+    "list": list,
+    "message": message,
+  };
+}
+
+/// imageid : "264237719"
+/// videoUrl : null
+/// materials : "玉米渣,大米,枸杞"
+/// name : "玉米渣粥"
+/// description : "玉米渣可以用来煮饭和煲粥，具有通便、护眼、降压降脂、补脑的功效。"
+/// recommend : ""
+/// id : "264237722"
+/// user : {"imageid":"264065389","star":0,"profile":"请添加您的个人简介","nickname":"薯条沾腐乳","signed":0,"id":"263277981","type":"Rookie"}
+/// infos : {"hasVideo":false,"grade":"1","collectionCount":41,"exclusive":0,"likeCount":37,"viewCount":668,"isrec":"4"}
+/// status : 1
+
+class ListBean {
+  String imageid;
+  dynamic videoUrl;
+  String materials;
+  String name;
+  String description;
+  String recommend;
+  String id;
+  UserBean user;
+  InfosBean infos;
+  int status;
+
+  static ListBean fromMap(Map<String, dynamic> map) {
+    if (map == null) return null;
+    ListBean listBean = ListBean();
+    listBean.imageid = map['imageid'];
+    listBean.videoUrl = map['videoUrl'];
+    listBean.materials = map['materials'];
+    listBean.name = map['name'];
+    listBean.description = map['description'];
+    listBean.recommend = map['recommend'];
+    listBean.id = map['id'];
+    listBean.user = UserBean.fromMap(map['user']);
+    listBean.infos = InfosBean.fromMap(map['infos']);
+    listBean.status = map['status'];
+    return listBean;
+  }
+
+  Map toJson() => {
+    "imageid": imageid,
+    "videoUrl": videoUrl,
+    "materials": materials,
+    "name": name,
+    "description": description,
+    "recommend": recommend,
+    "id": id,
+    "user": user,
+    "infos": infos,
+    "status": status,
+  };
+}
+
+/// hasVideo : false
+/// grade : "1"
+/// collectionCount : 41
+/// exclusive : 0
+/// likeCount : 37
+/// viewCount : 668
+/// isrec : "4"
+
+class InfosBean {
+  bool hasVideo;
+  String grade;
+  int collectionCount;
+  int exclusive;
+  int likeCount;
+  int viewCount;
+  String isrec;
+
+  static InfosBean fromMap(Map<String, dynamic> map) {
+    if (map == null) return null;
+    InfosBean infosBean = InfosBean();
+    infosBean.hasVideo = map['hasVideo'];
+    infosBean.grade = map['grade'];
+    infosBean.collectionCount = map['collectionCount'];
+    infosBean.exclusive = map['exclusive'];
+    infosBean.likeCount = map['likeCount'];
+    infosBean.viewCount = map['viewCount'];
+    infosBean.isrec = map['isrec'];
+    return infosBean;
+  }
+
+  Map toJson() => {
+    "hasVideo": hasVideo,
+    "grade": grade,
+    "collectionCount": collectionCount,
+    "exclusive": exclusive,
+    "likeCount": likeCount,
+    "viewCount": viewCount,
+    "isrec": isrec,
+  };
+}
+
+/// imageid : "264065389"
+/// star : 0
+/// profile : "请添加您的个人简介"
+/// nickname : "薯条沾腐乳"
+/// signed : 0
+/// id : "263277981"
+/// type : "Rookie"
+
+class UserBean {
+  String imageid;
+  int star;
+  String profile;
+  String nickname;
+  int signed;
+  String id;
+  String type;
+
+  static UserBean fromMap(Map<String, dynamic> map) {
+    if (map == null) return null;
+    UserBean userBean = UserBean();
+    userBean.imageid = map['imageid'];
+    userBean.star = map['star'];
+    userBean.profile = map['profile'];
+    userBean.nickname = map['nickname'];
+    userBean.signed = map['signed'];
+    userBean.id = map['id'];
+    userBean.type = map['type'];
+    return userBean;
+  }
+
+  Map toJson() => {
+    "imageid": imageid,
+    "star": star,
+    "profile": profile,
+    "nickname": nickname,
+    "signed": signed,
+    "id": id,
+    "type": type,
+  };
+}
