4b4ae4969276217b5a1b621507d44bfcce4ce974
maiya <maiya@ecook.cn>
2020-07-11 18:04:01 +0800

    misc:缓存课程代码

---
+++ reducing_cook/lib/model/menu_test_model.g.dart
@@ -0,0 +1,18 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'menu_test_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+DemoModel _$DemoModelFromJson(Map<String, dynamic> json) {
+  return DemoModel(
+      json['count'] as int, json['name'] as String, json['typeList'] as List);
+}
+
+Map<String, dynamic> _$DemoModelToJson(DemoModel instance) => <String, dynamic>{
+      'count': instance.count,
+      'name': instance.name,
+      'typeList': instance.typeList
+    };
